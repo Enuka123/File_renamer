85f311812ae9997eb8a031f6c8217a1ae5eb061c
Enuka Gayashan <enukagayashan123@gmail.com>
2025-04-23 21:41:50 +0530

    committed fix 2 globals.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,6 +8,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         border: "hsl(var(--border))",
       },
     },
